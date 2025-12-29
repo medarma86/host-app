c0491271d3195d5505370ae95a98d6ccc712e3e9
Madhu <medarama86@gmail.com>
2025-12-29 19:53:41 +0000

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', 
+    component: Home, // This will show when you click "Home"
+    pathMatch: 'full'
+  },
+    {
+    path: 'profile',
+    loadComponent: () => 
+      loadRemoteModule('remote-app', './ProfileView').then(m => m.ProfileView)
+  }
+];
